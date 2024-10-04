62cf703035bd635b68ae28f2dd6c704b7f67d985
Alex <eafloresc9@gmail.com>
2024-10-04 10:31:09 -0600

    Create a component for the menu

---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <router-link to="/" activeClass="border-b-2 border-indigo-50">
+    About
+  </router-link>
+  <router-link to="/projects" activeClass="border-b-2 border-indigo-50">
+    Projects
+  </router-link>
+  <router-link to="/faq" activeClass="border-b-2 border-indigo-50">FAQ</router-link>
+  <router-link to="/contact" activeClass="border-b-2 border-indigo-50">Contact</router-link>
+  <router-link to="/copycats" activeClass="border-b-2 border-indigo-50">Copycats</router-link>
+</template>
--- src/components/PortfolioComponent.vue
+++ src/components/PortfolioComponent.vue
@@ -1,20 +1,13 @@
  <script setup lang="ts">
 
-</script>
+import MenuComponent from "./MenuComponent.vue";
+ </script>
 
 <template>
   <div class="flex-grow flex flex-row justify-between mt-5 border-2 border-b-amber-50 p-5">
     <div class="flex items-center menu">
       <nav class="flex flex-col navbar navbar-expand-lg align-middle">
-        <router-link to="/" activeClass="border-b-2 border-indigo-50">
-          About
-        </router-link>
-        <router-link to="/projects" activeClass="border-b-2 border-indigo-50">
-          Projects
-        </router-link>
-        <router-link to="/faq" activeClass="border-b-2 border-indigo-50">FAQ</router-link>
-        <router-link to="/contact" activeClass="border-b-2 border-indigo-50">Contact</router-link>
-        <router-link to="/copycats" activeClass="border-b-2 border-indigo-50">Copycats</router-link>
+        <menu-component />
       </nav>
     </div>
     <div class="content">
