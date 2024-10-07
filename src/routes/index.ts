e45d6aa64c21d2b4af0f2c9eed28d5a7dc77043e
Alex <eafloresc9@gmail.com>
2024-10-07 14:54:19 -0600

    Change the web history to createWebHashHistory

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import About from "../pages/About.vue";
 import Projects from "../pages/Projects.vue";
 import Faq from "../pages/Faq.vue";
@@ -34,7 +34,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
